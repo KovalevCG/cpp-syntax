5c46bb7542e7750f82a5472bbe04cf58ab44aceb
Alexander Kovalev <kovalev.cg@gmail.com>
2022-02-15 22:25:59 +0200

    commit

--- main.cpp
+++ main.cpp
@@ -28,31 +28,81 @@ void dataTypes() {
 	unsigned int ui = 10; // unsigned integer
 	signed int si = -10;  //signed integer, same as (int)
 
-
 	// Textual Data Types
-	char c = 'a';
 	cout << endl << endl;
 	cout << "----------------=< Textual Data Types >=---------------" << endl;
+
+	char c = 'a';
 	cout << "Size of (char):  " << sizeof(c) << " byte(s)" << "     MIN: " << CHAR_MIN << "; MAX: " << CHAR_MAX << endl;
 	string st = "ABC";
 	cout << "Size of (string):  " << sizeof(st) << " byte(s)" << endl;
 
-
-
 	// Boolean Data Types
+	cout << endl << endl;
+	cout << "----------------=< Boolean Data Types >=---------------" << endl;
+
+	bool bl = true;
+	cout << "Size of (bool):  " << sizeof(bl) << " byte(s)" << endl;
+
+}
+
+void operators() {
+	
+	// Assignment Operators
+	cout << endl << endl;
+	cout << "----------------=< Assignment Operators >=---------------" << endl;
+	int a = 10;
+	cout << "a = " << a << ";   ++a = " << ++a << ";   a = " << a << endl;
+	a = 10;
+	cout << "a = " << a << ";   a++ = " << a++ << ";   a = " << a << endl;
+
+	// Logical Operators
+	cout << endl;
+	cout << "----------------=< Logical Operators >=---------------" << endl;
+	a = 10;
+	int b = 20;
+	cout << "a = 10; b = 20;\n";
+	cout << "(a < b) || (a > 15)  ==>  " << ((a < b) || (a > 15)) << endl; // Logical OR
+	cout << "(a < b) && (a > 15)  ==>  " << ((a < b) && (a > 15)) << endl; // Logical AND
+	cout << "!(a < b)  ==>  " << (! (a < b)) << endl; // Logical NOT
+
+	// Bitwise Operators
+	cout << endl;
+	cout << "----------------=< Bitwise Operators >=---------------" << endl;
+	a = 5; // (00000101)
+	b = 9; // (00001001)
+	cout << "a = " << a << "," << " b = " << b << endl;
+	cout << "a & b = " << (a & b) << endl;          // bitwise AND
+	cout << "a | b = " << (a | b) << endl;          // bitwise OR
+	cout << "a ^ b = " << (a ^ b) << endl;          // bitwise XOR
+	cout << "~(a) = " << (~a) << endl;              // bitwise Complement
+	cout << "b << 1" << " = " << (b << 1) << endl;  // bitwise Shift Left
+	cout << "b >> 1 " << "= " << (b >> 1) << endl;  // bitwise Shift Right
+
+
+}
+
+void userInput() {
+	string name;
+	cout << "Please enter your name: " << endl;
+	cin >> name;
+	cout << "Hello " << name << "!" << endl;
 }
 
 int main() {
 
-	/*
-	
+
 	// Function that prints "Hello" + string argument + "!!!" to the console
-	coutHello("C++");
+	//coutHello("C++");
 
-	*/
+	// Function that prints size of different data types
+	//dataTypes();
 
-	// Function
-	dataTypes();
+	// Operators
+	//operators();
+
+	// User Input
+	userInput();
 
 	return 0;
 }
